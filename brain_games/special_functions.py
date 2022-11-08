935eab0a73db647cdc8b8d15ac199aeee5ede976
Elenlith <mishchenkov@list.ru>
2022-11-08 13:10:31 +0300

    revise is_prime_number() function to reduce cognitive complexity

--- brain_games/special_functions.py
+++ brain_games/special_functions.py
@@ -56,18 +56,23 @@ def hide_progression_element(progression, hidden_element):
     return progression_with_hidden_element
 
 
+def is_prime_number_cycle(number):
+    right_answer = 'yes'
+    i = 2
+    while i < number:
+        if number % i == 0:
+            right_answer = 'no'
+            break
+        else:
+            i += 1
+    return right_answer
+
+
 def is_prime_number(number):
     if number == 1:
         right_answer = 'no'
     elif number == 2:
         right_answer = 'yes'
     else:
-        right_answer = 'yes'
-        i = 2
-        while i < number:
-            if number % i == 0:
-                right_answer = 'no'
-                break
-            else:
-                i += 1
+        right_answer = is_prime_number_cycle(number)
     return right_answer
